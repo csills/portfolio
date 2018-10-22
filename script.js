e0c9955e44c07ac1c08af0e1d28275e2bbc5729d
csills <sillscrystal@gmail.com>
2018-10-22 11:18:32 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// When the user clicks on the scroll top arrow button, scroll to the top of the document.
+function scrolltopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
