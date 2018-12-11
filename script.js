da47f37ce43d3b8f1cd161e937b24e17b59e86aa
csills <sillscrystal@gmail.com>
2018-12-11 10:58:08 -0500

    adding contactFormParser

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ function scrolltopFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+// Submit Form Button //
+
+// use underscore instead of getElementById in the code:
+function _(id) { return document.getElementById(id); }
+
+function submitForm(){
+	_("submitButton").disabled = true;
+	_("status").innerHTML = 'ting  please wait ...';
+	var formdata = new FormData();
+	formdata.append( "inputName", _("inputName").value );
+	formdata.append( "inputEmail", _("inputEmail").value );
+	formdata.append( "inputMessage", _("inputMessage").value );
+	var ajax = new XMLHttpRequest();
+	ajax.open( "POST", "contactFormParser.php" );
+	ajax.onreadystatechange = function() {
+		if(ajax.readyState == 4 && ajax.status == 200) {
+			if(ajax.responseText == "success"){
+				_("submit_form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
+			} else {
+				_("status").innerHTML = ajax.responseText;
+				_("submitButton").disabled = false;
+			}
+		}
+	}
+	ajax.send( formdata );
+}
